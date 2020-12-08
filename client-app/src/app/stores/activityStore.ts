82c338eb30fa666fa304a4aa1d09293b18170eca
janizei <jani.niemisto@live.fi>
2020-12-08 11:40:53 +0200

    End of lecture 85

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,57 @@
+import { observable, action, makeObservable } from 'mobx';
+import { createContext } from 'react';
+import agent from '../api/agent';
+import { IActivity } from '../models/activity';
+
+class ActivityStore {
+
+    constructor() {
+        makeObservable(this);
+    }
+
+    @observable activities: IActivity[] = [];
+    @observable selectedActivity: IActivity | undefined;
+    @observable loadingInitial = false;
+    @observable editMode = false;
+    @observable submitting = false;
+
+    @action loadActivities = async () => {
+        this.loadingInitial = true;
+        try{
+        const activities = await agent.Activities.list();
+        activities.forEach((activity) => {
+          activity.date = activity.date.split(".")[0];
+          this.activities.push(activity);
+        });
+        this.loadingInitial = false;
+        } catch (error) {
+            console.log(error);
+            this.loadingInitial = false;
+        }
+    };
+
+    @action createActivity = async (activity: IActivity) => {
+        this.submitting = true;
+        try {
+            await agent.Activities.update(activity);
+            this.activities.push(activity);
+            this.editMode = false;
+            this.submitting = false;
+        } catch (error) {
+            this.submitting = false;
+            console.log(error);
+        }
+    };
+
+    @action openCreateForm = () => {
+        this.editMode = true;
+        this.selectedActivity = undefined;
+    }
+
+    @action selectActivity = (id: string) => {
+        this.selectedActivity = this.activities.find(a => a.id === id);
+        this.editMode = false;
+    }
+}
+
+export default createContext(new ActivityStore());
