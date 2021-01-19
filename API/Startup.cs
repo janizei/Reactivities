192b349dcf323175562d3a55d5c9d5423d88e063
janizei <jani.niemisto@live.fi>
2021-01-19 02:32:06 +0200

    End of section 16

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,9 +1,11 @@
 using API.Middleware;
 using Application.Activities;
 using Application.Interfaces;
+using Application.Photos;
 using AutoMapper;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Photos;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -96,6 +98,8 @@ namespace API
 
             services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
